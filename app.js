8d0d368b17d4fb929e9fa8afc4d56fc9c1bf183c
atul <atul.excel2011@gmail.com>
2019-05-14 11:34:51 +0530

    update for cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eventRouter = require('./routes/Events');
 var app = express();
-
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
